e862c4fa258703281e163c32a87f326c8eeda24b
csandreas1 <andreas.3@windowslive.com>
2017-07-29 16:44:45 +0300

    Add files via upload

--- js.js
+++ js.js
@@ -1,8 +1,8 @@
            $( document ).ready(function() {
      var c1Title= $('#r1c1Title').text();
  
-    
-     localStorage.setItem("t1",c1Title);
+                          
+     localStorage.setItem("t1",c1Title);//changing c1Title to any String content like "test" will work
     var result = localStorage.getItem("t1");
     
     $("#title1").html(result);
