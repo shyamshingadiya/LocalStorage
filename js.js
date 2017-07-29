c23239a8af3728a35719919401c1b4ffc366b970
Shyam Shingadiya <shyamshingadiya@users.noreply.github.com>
2017-07-29 23:03:31 +0530

    Update js.js

--- js.js
+++ js.js
@@ -1,10 +1,3 @@
-           $( document ).ready(function() {
-     var c1Title= $('#r1c1Title').text();
- 
-                          
-     localStorage.setItem("t1",c1Title);//changing c1Title to any String content like "test" will work
     var result = localStorage.getItem("t1");
     
     $("#title1").html(result);
-    alert(result);
-});
